12fa5f88b6c5254ee1fcaaaabe6386d2ca72fa26
helvisklei <helvisklei@hotmail.com>
2021-08-26 16:07:35 -0300

    criando o menu de contact

--- app/src/main/java/hvkproducao/com/br/contact/MainActivity.kt
+++ app/src/main/java/hvkproducao/com/br/contact/MainActivity.kt
@@ -2,6 +2,11 @@ package hvkproducao.com.br.contact
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -39,5 +44,27 @@ class MainActivity : AppCompatActivity() {
             )
         )
 
+    }
+    // recebe o menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+       val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu,menu)
+        return true
+    }
+    private fun showToast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.item_menu_1 ->{
+                showToast("Exibindo item de menu 1")
+                true
+            }
+            R.id.item_menu_2 ->{
+                showToast("Exibindo item de menu 1")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
